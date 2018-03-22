7022efc305b8caef9e90aeb6fec5264ad8a2c86e
ujjwal <pantaujjwal49@gmail.com>
2018-03-22 08:26:03 +0545

    added copy of Addition

---
+++ Addition - Copy.java
@@ -0,0 +1,15 @@
+class Addition
+{
+	public static void main(String[] args) 
+	{
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		System.out.println("your first input"+a);
+		System.out.println("your second input"+b);
+		System.out.println("the sum is"+(a+b));
+		System.out.println("the multiplication is"+(a*b));
+		System.out.println("the division is"+(a/b));
+		System.out.println("the difference between the numbers is"+(a-b));
+
+	}
+}
